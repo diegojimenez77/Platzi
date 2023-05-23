369bc0dd3485300d48138e88051a8118469f3588
diegojimenez77 <djpcoding8652@gmail.com>
2023-05-22 20:34:00 -0600

    completing clase 3

--- Curso_Intermedio_Programacion_Orientada_Objetos_Javascritp/Calse_3/main.js
+++ Curso_Intermedio_Programacion_Orientada_Objetos_Javascritp/Calse_3/main.js
@@ -1,8 +1,11 @@
+// En este ejemplo se muestran las diferentes formas que podemos obtener la informacion de un objeto.
 const juan = {
     name: "juanito",
     age: 18,
     approvedCourses: ["Curso 1"],
     addCourse(newCourse) {
+        console.log("This", this);
+        console.log("This.approvedCourses", this.approvedCourses);
         this.approvedCourses.push(newCourse);
     }
 };
@@ -10,3 +13,12 @@ const juan = {
 console.log(Object.keys(juan));
 console.log(Object.getOwnPropertyNames(juan));
 console.log(Object.entries(juan));
+
+console.log(Object.getOwnPropertyDescriptors(juan));
+
+console.log(Object.defineProperty(juan, "pruebaNASA", {
+    value:"extraterrestres",
+    enumerable: true,
+    writable: true,
+    configurable: true,
+}));
