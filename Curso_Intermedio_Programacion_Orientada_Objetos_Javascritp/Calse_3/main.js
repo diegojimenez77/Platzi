214bab2a1c458c2024e3133e765fa00cff8fd774
diegojimenez77 <djpcoding8652@gmail.com>
2023-05-22 21:01:07 -0600

    adding seal and freeze properties

--- Curso_Intermedio_Programacion_Orientada_Objetos_Javascritp/Calse_3/main.js
+++ Curso_Intermedio_Programacion_Orientada_Objetos_Javascritp/Calse_3/main.js
@@ -16,9 +16,41 @@ console.log(Object.entries(juan));
 
 console.log(Object.getOwnPropertyDescriptors(juan));
 
+//Cons estos ejemplos entenderemos como es que funcionan los atributos de las propiedades
 console.log(Object.defineProperty(juan, "pruebaNASA", {
     value:"extraterrestres",
-    enumerable: true,
+    enumerable: false,
+    writable: false,
+    configurable: false,
+}));
+
+console.log(Object.defineProperty(juan, "navigator", {
+    value:"Chrome",
+    enumerable: false,
     writable: true,
     configurable: true,
 }));
+
+
+console.log(Object.defineProperty(juan, "editor", {
+    value:"VSCode",
+    enumerable: true,
+    writable: false,
+    configurable: true,
+}));
+
+
+console.log(Object.defineProperty(juan, "terminal", {
+    value:"WSL",
+    enumerable: true,
+    writable: true,
+    configurable: false,
+}));
+
+//Otros metodos estaticos del superprotitupo Object
+
+Object.seal(juan); //pone todas las propiedades configurable como false
+Object.freeze(juan); //pone todas las propiedades writable como false, tampoco permite modificarlas.
+
+
+console.log(Object.getOwnPropertyDescriptors(juan));
